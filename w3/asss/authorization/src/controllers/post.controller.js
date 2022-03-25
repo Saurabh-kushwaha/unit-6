bc394eda6ee3c48f8aa887f97cf583c387ca907d
Saurabh-kushwaha <kushwahasaurabh219@gmail.com>
2022-03-25 12:12:17 +0530

    authentication

---
+++ w3/asss/authorization/src/controllers/post.controller.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const authenticate = require("../middlewares/authenticate.middleware");
+const authorise = require("../middlewares/authorise.middleware");
+
+const Product = require("../models/post.model");
+
+const router = express.Router();
+
+router.post("", authenticate, async (req, res) => {
+  try {
+    const product = await Product.create({
+      ...req.body,
+      user_id: req.user._id,
+    });
+
+    return res.send(product);
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+});
+
+router.get("", authenticate, async (req, res) => {
+  try {
+    const products = await Product.find();
+
+    return res.send(products);
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+});
+
+router.get("/my", authenticate, async (req, res) => {
+  try {
+    const products = await Product.find({ user_id: req.user._id });
+
+    return res.send(products);
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+});
+
+router.patch("/:id", authenticate, authorise(["owner"]), async (req, res) => {
+  try {
+    const product = await Product.findByIdAndUpdate(
+      req.params.id,
+      {
+        ...req.body,
+        user_id: req.user._id,
+      },
+      { new: true }
+    );
+
+    return res.send(product);
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+});
+
+module.exports = router;
